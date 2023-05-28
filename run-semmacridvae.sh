99ba48946cdddf8e6182313e652c04ac1f687d49
Ignacio Avas <joseignacio.avas@student.kuleuven.be>
2023-05-28 18:44:39 +0200

    Modify run-semmacridvae.sh

--- run-semmacridvae.sh
+++ run-semmacridvae.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash -l
 set -e
 
+
 echo "Exporting variables"
 export PATH="${VSC_DATA}/cuda11.7/bin:${PATH}"
 export LD_LIBRARY_PATH="${VSC_DATA}/cuda-11.7/lib64:${LD_LIBRARY_PATH}"
@@ -11,9 +12,6 @@ conda activate semmacridvae
 echo "Checking for GPU in Torch"
 echo 'import torch; torch.cuda.current_device()' | python
 
-echo "Running main.py"
-export BETA=0.001
-echo "Running main with BETA=$BETA"
-python main.py --data amazon-clothing --model DisenEVAE --epochs 50 --batch_size 100 --device cuda --beta $BETA
-echo "Running Uncorrelate with  $BETA"
-python main.py --data amazon-clothing --model DisenEVAE --epochs 50 --batch_size 100 --device cuda --beta $BETA --mode uncorrelate
+echo "Running main.py with extra arguments $@"
+python main.py --data amazon-clothing --model DisenEVAE --epochs 50 --batch_size 100 --device cuda $@
+
